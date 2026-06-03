37d97a30948d65c41dd4b62783b2e59d39735f4d
Tanupriya91 <tp6677@srmist.edu.in>
2026-06-03 14:13:20 +0530

    index.js and firebase.js has been updated

---
+++ config/firebase.js
@@ -0,0 +1,14 @@
+const admin = require("firebase-admin");
+const serviceAccount = require("../firebase/serviceAccountKey.json");
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+
+const db = admin.firestore();
+console.log("Firebase Project:",serviceAccount.project_id);
+
+module.exports= {
+    admin,
+    db,
+};
--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 const express = require ("express");
 
+const {db} = require("./config/firebase"); 
+
 const app = express();
 
 app.use(express.json());
 
 app.get("/", (req,res) => {
      res.send("Expense Tracker API");
+});
+
+app.get("/test", async (req,res) => {
+    try{
+        const collection = await db.listCollections();
+
+        res.status(200).json({
+            success:true,
+            collection: collection.map((c)=>c.id),
+        });
+
+    }
+    catch(error){
+        res.status(500).json({
+            success: false,
+            error: error.message,
+        });
+    }
 });
 app.listen(3000, ()=>{
     console.log("Server is running on port 3000");
